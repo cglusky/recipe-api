100c3b7d2642c295d392602aa93cd41cc47d10e3
cglusky <43158+cglusky@users.noreply.github.com>
2020-07-14 13:34:52 -0500

    added recipe feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -50,3 +50,11 @@ class ModelTests(TestCase):
         ingredient = models.Ingredient.objects.create(user=test_user(), name="Cucumber")
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test recipe str output"""
+        recipe = models.Recipe.objects.create(
+            user=test_user(), title="Veggie Stir Fry", time_minutes=5, price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
